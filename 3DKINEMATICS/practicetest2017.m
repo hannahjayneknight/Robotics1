4273a92ebfba311239941aa9882e7f42c984d154
Hannah Jayne Knight <hannah@tinpot.co.uk>
2021-12-14 09:27:54 +0000

    formatting 2017 paper

---
+++ 3DKINEMATICS/practicetest2017.m
@@ -0,0 +1,35 @@
+%Question 2a)
+
+clear all; 
+close all; 
+clc; 
+
+
+%% 
+%Question 2b)
+
+clear all; 
+close all; 
+clc; 
+
+
+%% 
+%Question 2c)
+
+clear all; 
+close all; 
+clc; 
+
+
+
+%%
+
+% My choice is 3
+
+%—– Explanation with a Matlab code——-
+% Please run the following Matlab code with commented lines to verify my answer
+
+clear all; 
+close all; 
+clc; 
+
