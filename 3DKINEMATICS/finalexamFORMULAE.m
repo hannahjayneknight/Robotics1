52e609b95bce8c4bab1cf2624f0de0ad9a410663
Hannah Jayne Knight <hannah@tinpot.co.uk>
2021-12-14 09:38:51 +0000

    adding transformation matrix

--- 3DKINEMATICS/finalexamFORMULAE.m
+++ 3DKINEMATICS/finalexamFORMULAE.m
@@ -1,4 +1,15 @@
 % FINAL EXAM FORMULAE
 
 %% 
-%Transformation matrix
+%Transformation matrix 
+% replace i with numbers according to which transformation, then multiply 
+% all together to get the homogenous matrix
+
+syms thetai alphai di ai;
+jTi = [ cos(thetai), -sin(thetai), 0, ai;
+        sin(thetai)*cos(alphai), cos(thetai)*cos(alphai), -sin(alphai), -sin(alphai)*di;
+        sin(thetai)*sin(alphai), cos(thetai)*sin(alphai), cos(alphai), cos(alphai)*di;
+        0, 0, 0, 1 ];
+
+%% 
+%Jacobian stuff
